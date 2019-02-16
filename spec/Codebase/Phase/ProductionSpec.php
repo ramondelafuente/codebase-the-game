9adf9351de658eeeb89aee46e65d403176e92998
Ramon de la Fuente <ramon@future500.nl>
2019-02-16 12:51:28 +0100

    Add codebase to production phase

--- spec/Codebase/Phase/ProductionSpec.php
+++ spec/Codebase/Phase/ProductionSpec.php
@@ -2,12 +2,15 @@
 
 namespace Codebase\Phase;
 
+use Codebase\Codebase;
 use PhpSpec\ObjectBehavior;
 
 class ProductionSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedThrough('plan', [Codebase::initialize()]);
+
         $this->shouldHaveType(Production::class);
     }
 }
--- src/Codebase/Phase/Production.php
+++ src/Codebase/Phase/Production.php
@@ -3,6 +3,24 @@ declare(strict_types=1);
 
 namespace Codebase\Phase;
 
-class Production
+use Codebase\Codebase;
+
+final class Production
 {
+    /**
+     * @var Codebase
+     */
+    private $codebase;
+
+    private function __construct()
+    {
+    }
+
+    public static function plan(Codebase $codebase): self
+    {
+        $production = new Production();
+        $production->codebase  = $codebase;
+
+        return $production;
+    }
 }
